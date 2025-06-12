3e6ff166090ee6935e0d53f3e2bec9d9f483f356
Jerome Barbato <jerome.barbato@gmail.com>
2025-06-12 23:08:45 +0200

    update doc

--- vite.config.js
+++ vite.config.js
@@ -24,13 +24,7 @@ export default defineConfig(({ command }) =>{
     resolve: {
       alias:[
           { find: '@', replacement: path.resolve(__dirname, 'assets') },
-          { find: 'vue', replacement: 'vue/dist/vue.esm-bundler.js' }
       ]
-    },
-    define: {
-      __VUE_OPTIONS_API__: true,
-      __VUE_PROD_DEVTOOLS__: command!=='build',
-      __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: command!=='build'
     },
     base: '/build',
     css: {
